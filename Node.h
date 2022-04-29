6c891572b1fdacc8777a68f6e877efdcbbc643e4
HectorMau501 <mauhector7@gmail.com>
2022-04-29 16:37:09 -0500

    Corrección del programa

--- Node.h
+++ Node.h
@@ -1,48 +1,120 @@
 #pragma once
 #include <iostream>
 
-//template <typename T>
-
-/*
--------------- Profe quise hacerlo con templates, pero no pude porque me marcaba error en algunos puntos
-lo hice de esta manera, deje una forma con comentarios como lo quise hacer con los templates
-pero como le digo me salian algunos errores-----------------------------
-
-
-   --- Lo hice de otra forma ---
-*/
+template <typename T>
 
 
 class Node
 {
 
 private:
-    /*
-    T value = 0;
+    
+    T value;
     Node* son = nullptr;
-    */
-
-    int note = 0;
-    Node* right = nullptr;
-    Node* left = nullptr;
-    
-    //typedef Node* ptrPila;
-
-    /*
-    
-    size_t tam;
-    size_t cont;
-
-    const static size_t MAX = 5;
-
-    void expandir();
-
-    */
+   
 
 public:
 
+    Node(){}//Constructor
+    ~Node(){}//Destructor
 
-    /*
+
+    //Constructor 
+    Node(const T& newValue)
+    {
+        value = newValue;
+    }
+
+
+    //Metodo insertarvalor
+    void addValue(const T& newValue)
+    {
+        if (son == nullptr)
+        {
+            son = new Node<T>(newValue);
+        }
+        else 
+        {
+            son->addValue(newValue);
+        }
+
+    }
+    //Puntero siguiente
+    Node* next() 
+    {
+        return this->son;
+    }
+
+    //Metodo colocar valor
+    void setValue(const T& newValue) { value = newValue;}
+    T& getValue() 
+    {
+        return value;
+    }
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+//Se crea el nuevo nodo
+Node* createNode(int p)
+{
+    Node* newNode = new Node();
+    newNode->note = p;
+    return newNode;
+}
+
+
+//Se crea la funcio add para integrar el valor a la estructura de datos y despues darle un valor al nodo
+void add(Node*& source, int value)
+{
+    //Aqui se obtiene el valor de la estructura para despues realizarla
+    if (source == NULL)
+    {
+        Node* newNode = createNode(value);
+        source = newNode;
+        std::cout << "\t\t\nSe realiza la estructura de datos " << value << std::endl;
+    }
+    else
+    {
+
+        int newSource = source->note;
+        //Aqui se ve que se añade el valor por la izquierda o derecha
+        if (value < newSource)
+        {
+            add(source->left, value);
+            std::cout << "\t\t\nSe integra " << value << " el valor por parte de la izquierda " << std::endl;
+        }
+        else
+        {
+            add(source->right, value);
+            std::cout << "\t\t\nSe integra " << value << " el valor por parte de la derecha " << std::endl;
+        }
+
+    }
+}
+
+
+
+    */
+
+
+/*
+
+  int note = 0;
+
 
 //Constructor
 template <typename T>
@@ -65,49 +137,27 @@ Node<T>::~Node()
 */
 
 
-    Node(){}//Constructor
-    ~Node(){}//Destructor
+/*
+ Node* right = nullptr;
+ Node* left = nullptr;
+ */
 
-    //Se crea el nuevo nodo
-    Node *createNode(int p)
-    {
-        Node* newNode = new Node();
-        newNode->note = p;
-        return newNode;
-    }
+ //typedef Node* ptrPila;
+
+ /*
+
+ size_t tam;
+ size_t cont;
+
+ const static size_t MAX = 5;
+
+ void expandir();
+
+ */
 
 
-    //Se crea la funcio add para integrar el valor a la estructura de datos y despues darle un valor al nodo
-    void add(Node*&source, int value)
-    {
-        //Aqui se obtiene el valor de la estructura para despues realizarla
-        if (source == NULL)
-        {
-            Node* newNode = createNode(value);
-            source = newNode;
-            std::cout << "\t\t\nSe realiza la estructura de datos " <<  value << std:: endl;
-        }
-        else
-        {
-           
-            int newSource = source->note;
-            //Aqui se ve que se añade el valor por la izquierda o derecha
-            if(value < newSource)
-            {
-                add(source->left, value);
-                std::cout << "\t\t\nSe integra " << value << " el valor por parte de la izquierda " << std::endl;
-            }
-            else
-            {
-                add(source->right, value);
-                std::cout << "\t\t\nSe integra " << value << " el valor por parte de la derecha " << std::endl;
-            }
+/*
 
-        }
-    }
-
-    /*
-    
     template <typename T>
     void Node<T>::InsertarFinal(const T& newValue)
     {
@@ -152,97 +202,6 @@ Node<T>::~Node()
 
     */
 
-
-
-
-
-
-    
-
-    /*
-    void InsertarFinal(const T& newValue);
-    void InsertarInicio(const T& newValue);
-    
-    size_t size();
-    
-    int operator[](size_t p)
-    {
-        return son[p];
-    }
-    */
-
-
-    /*
-    //Insertar elemento en la pila
-    void Insertar(const T& newValue)
-    {
-        ptrPila aux;
-        aux = new (class Node); //Apuntando al  nuevo nodo
-        aux->value = newValue;
-        aux->son = T;
-        T = aux;
-    }
-
-    //Borrar elemento en la pila
-
-    Node* Borrar(ptrPila T&)
-    {
-        T value = 0;
-        ptrPila aux;
-
-        aux = T;
-        son = aux->value;
-
-        T = aux->son;
-        delete(aux);
-
-        return value;
-    }
-
-    void Listar_pila(ptrPila T)
-    {
-        ptrPila aux;
-        aux = T;
-
-        while(aux != NULL)
-        {
-            std::cout << "\t" << aux->value << endl;
-            aux = aux->son;
-        }
-
-    }
-
-    Node(const T& newValue)
-    {
-        value = newValue;
-    }
-
-    void addValue(const T& newValue)
-    {
-        if (son == nullptr)
-        {
-            son = new Node<int>(newValue);
-        }
-        else 
-        {
-            son->addValue(newValue);
-        }
-
-    }
-
-    Node* next() 
-    {
-        return this->son;
-    }
-
-    void setValue(const T& newValue) { value = newValue; }
-    T& getValue() 
-    {
-        return value;
-    }
-   */
-
-};
 
 
 
--- Proyecto2.cpp
+++ Proyecto2.cpp
@@ -1,19 +1,21 @@
 // Proyecto2.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
 //
 
- #include <iostream>
+#include <iostream>
+#include <string>
 #include <vector>
 #include "Node.h"
 
-/*template <typename T1>
+
+
+/*
+template <typename T1>
 const T1&  getMax(const T1& param1, const T1& param2)
 {
     if(param1 > param2)
         return param1;
     return param2;
-
 }
-
 
 int  getMaxEnteros(int  param1, int param2)
 {
@@ -21,80 +23,152 @@ int  getMaxEnteros(int  param1, int param2)
         return param1;
     return param2;
 
-}*/
+}
+*/
+
+
 
 
 int main() {
 
     int option = 0;
-    int value = 0;
-    //punteros 
-    Node* p = nullptr;
-    Node* source = nullptr;
-    
-    
+    int opc = 0;
+    int valor;
+    float value;
+    std::string write;
+    bool x;
+
+   
     std::cout << "Binvenido a una Lista" << std::endl;
     std::cout << "Hector Mauricio Rodriguez Salazar 21310416" << std::endl;
     std::cout << "Este es un programa que se inserta y se elimina" << std::endl;
+    /*
     std::cout << "Ingrese el valor para la estructura de datos" << std::endl;
-    std::cin >> value;
-    //Creacion del nodo y uso del metodo add
-    source->createNode(value);
-    p->add(source,value);
+    std::cin >> value;*/
+
 
     while (option != 2)
     {
         std::cout << "Usted debera escoger una de las opciones" << std::endl;
         std::cout << "1.Agregar algun elemento al nodo" << std::endl;
         std::cout << "2.Salir" << std::endl;
-        std::cin>>option;
+        std::cin >> option;
 
         switch (option)
         {
-        case 1:
-            std::cout << "\t\t\nIngrese el valor del node" << std::endl;
-            std::cin >> value; 
-            //Creacion del nodo y uso del metodo add
-            source->createNode(value);
-            p->add(source, value);
+            case 1:
+
+                std::cout << "\t\t\nDe que tipo desea realizarlo?" << std::endl;
+                std::cout << "\t\t\n1.De tipo entero" << std::endl;
+                std::cout << "\t\t\n2.De tipo float" << std::endl;
+                std::cout << "\t\t\n3.De tipo string" << std::endl;
+                std::cout << "\t\t\n4.De tipo Booleano" << std::endl;
+                std::cin >> opc;
+            
+
+            switch (opc)
+            {
+                case 1:
+                    if (opc == 1) 
+                    {
+                        std::cout << "\t\t\n tipo entero" << std::endl;
+                        std::cin >> valor;
+                        Node<int> a(valor);
+                        a.addValue(valor);
+                        std::cout << "\n" << std::endl;
+                        std::cout << " Valor " << valor << " Agregado\n\n" << std::endl;
+                        break;
+                    }
+
+                case 2:
+                    if (opc == 2)
+                    {
+                        std::cout << "\t\t\n tipo flotante" << std::endl;
+                        std::cin >> value;
+                        Node<float> b(value);
+                        b.addValue(value);
+                        std::cout << "\n" << std::endl;
+                        std::cout << " Valor " << value << " Agregado\n\n" << std::endl;
+                    }
+                    break;
+                case 3:
+                    if (opc == 3)
+                    {
+                        std::cout << "\t\t\n tipo string (que no tenga espacios)" << std::endl;
+                        std::cin >> write;
+                        Node<std::string> c(write);
+                        c.addValue(write);
+                        std::cout << "\n" << std::endl;
+                        std::cout << " Escrito " << write << " Agregado\n\n" << std::endl;
+                    }
+                    break;
+                case 4:
+                    if (opc == 4)
+                    {
+                        std::cout << "\t\t\n tipo Booleano (1 o 0)" << std::endl;
+                        std::cin >> x;
+                        Node<bool> d(x);
+                        d.addValue(x);
+                        std::cout << "\n" << std::endl;
+                        std::cout << " Booleano " << x << " Agregado\n\n" << std::endl;
+                    }
+                    break;
+            }
+            
 
             break;
-        case 2:
-            std::cout << "\t\t\nVuelva pronto:)" << std::endl;
 
-            default:std::cout << "\t\t\nEscribio incorrecto el numero del menu" << std::endl;
+            case 2:
+
+                std::cout << "\t\t\nVuelva pronto:)" << std::endl;
+                break;
+
+            default:std::cout << "\t\t\nEscribio un numero incorrecto al del menu\n\n" << std::endl;
         }
-
+         
     }
     
 
 
-    /*std::cout<< getMax(1,2)<<std::endl;
+
+
+    /*
+    std::cin >> valor;
+    b.addValue(valor);
+    b.addValue(5);
+    b.addValue(6);
+    
+
+    std::cout << b.getValue() << std::endl;
+    
+    
+    std::cin >> write;
+    Node<std::string> a(write);
+    a.addValue("Que onda");
+    a.addValue("Quiuvoles");
+
+    std::cout << a.getValue() << std::endl;
+    
+
+        
+    std::cout<< getMax(1,2)<<std::endl;
     double a = 3;
     double b = 3.14156;
     std::cout<< getMax(a,b)<<std::endl;
-    std::cout<< getMaxEnteros(a,b)<<std::endl;*/
+    std::cout<< getMaxEnteros(a,b)<<std::endl;
 
-    /*
+    
     vector <int> a;
     a.push_back(7);
     a.push_back(2);
     a.push_back(3);
 
-    for (auto i = a.begin(); i != a.end(); i++) 
+    for (auto i = a.begin(); i != a.end(); i++)
     {
         cout << *i << endl;
     }
     
-
     
-    Node<int> b(1);
-
-    b.addValue(5);
-    b.addValue(6);
-
-    
-
     Node<int>* dummy = nullptr;
     dummy = &b;
     
@@ -104,13 +178,41 @@ int main() {
         cout << dummy->getValue() << endl;
         dummy = dummy->next();
     }
-    */
-   
+    
+   */
+
 
 
     system("pause");
     return 1;
 }
+
+
+
+
+
+
+
+//punteros 
+/*
+ Node* p = nullptr;
+ Node* source = nullptr;
+ */
+
+
+ /*Creacion del nodo y uso del metodo add
+ source->createNode(value);
+ p->add(source,value);
+ */
+
+
+ //Creacion del nodo y uso del metodo add
+ /*
+ source->createNode(value);
+ p->add(source, value);
+ */
+
+
 
 // Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
 // Depurar programa: F5 o menú Depurar > Iniciar depuración
